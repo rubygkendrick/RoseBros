d2be73783edfb6fd3df14ee093e2fe4f84e8745f
Ruby Kendrick <kendrick.ruby@gmail.com>
2024-07-01 13:47:54 -0500

    user is able to search for a rose based on color

--- client/src/components/rose/AllRoses.jsx
+++ client/src/components/rose/AllRoses.jsx
@@ -1,35 +1,64 @@
 import { useState, useEffect } from "react";
 import "./AllRoses.css"
 import { Link } from "react-router-dom";
-import { Card, CardBody, CardText, CardImg, Row, Col } from "reactstrap";
+import { Card, CardBody, CardText, CardImg, Row, Col, Label, Input } from "reactstrap";
 import { getRoses } from "../../managers/roseManager";
-
-
+import { getColors } from "../../managers/colorManager";
 
 
 export default function AllRoses() {
     const [roses, setRoses] = useState([]);
-
+    const [colors, setColors] = useState([]);
+    const [filteredRoses, setFilteredRoses] = useState([]);
+    const [colorId, setColorId] = useState(0);
 
     const getAndResetAllRoses = () => {
         getRoses().then(setRoses);
     };
 
-
+    const getAndSetColors = () => {
+        getColors().then(setColors);
+    };
 
     useEffect(() => {
         getAndResetAllRoses();
+        getAndSetColors();
     }, []);
 
 
+    const handleColorSelect = (event) => {
+        const selectedColorId = event.target.value;
+        setFilteredRoses(roses.filter(r => r.colorId == selectedColorId));
+    }
 
+
+    useEffect(() => {
+        setRoses(filteredRoses)
+    }, []);
+
+    const rosesToDisplay = filteredRoses.length > 0 ? filteredRoses : roses;
 
 
     return (
         <>
             <h1>RoseBros</h1>
+
+            <Input
+                type="select"
+                id="colorSearch"
+                className="input-field"
+                defaultValue=""
+                onChange={handleColorSelect}
+            >
+                <option value="">select a color</option>
+                {colors.map((color) => (
+                    <option key={color.id} value={color.id}>
+                        {color.name}
+                    </option>
+                ))}
+            </Input>
             <Row>
-                {roses.map((rose) => {
+                {rosesToDisplay.map((rose) => {
                     return (
                         <Col sm="12" md="6" lg="4" key={rose.id}>
                             <Card className="my-3">
